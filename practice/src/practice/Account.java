15723d0810590a7425ae16661403ad096791139c
user <user@192.168.1.6>
2021-07-20 15:59:41 +0530

    session commit1

---
+++ practice/src/practice/Account.java
@@ -0,0 +1,45 @@
+package practice;
+
+public class Account {
+	int accountNum;
+	String accountName;
+	int balance;
+	/**
+	 * @return the accountNum
+	 */
+	public int getAccountNum() {
+		return accountNum;
+	}
+	/**
+	 * @param accountNum the accountNum to set
+	 */
+	public void setAccountNum(int accountNum) {
+		this.accountNum = accountNum;
+	}
+	/**
+	 * @return the accountName
+	 */
+	public String getAccountName() {
+		return accountName;
+	}
+	/**
+	 * @param accountName the accountName to set
+	 */
+	public void setAccountName(String accountName) {
+		this.accountName = accountName;
+	}
+	/**
+	 * @return the balance
+	 */
+	public int getBalance() {
+		return balance;
+	}
+	/**
+	 * @param balance the balance to set
+	 */
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+	
+
+}
--- practice/src/practice/test.java
+++ practice/src/practice/test.java
@@ -1,10 +1,29 @@
 package practice;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world");
-	}
+		Connection con;
+		try {
+			Class.forName(null);
+			Statement stmt = con.createStatement(0, 0);
+			ResultSet rs = stmt.executeQuery("Select empid from emp");
+			while (rs.next()) {
+				rs.getInt(0);
+			}
+		} catch (Exception SQLException) {
 
+		} finally {
+			con.close();
+		}
+		
+		Account acc = new Account();
+		acc.setAccountName("ABC");
+	}
 }
